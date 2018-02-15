ec572ff433e758c7c5bc681f44924c447969b08d
acer-pc <acer-pc@LAPTOP-QKVEU2MK>
2018-02-15 17:42:43 +0800

    initial

---
+++ src/main/java/com/coco/aws/LearnAwsApplication.java
@@ -0,0 +1,12 @@
+package com.coco.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAwsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/coco/aws/LearnAwsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coco.aws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnAwsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
